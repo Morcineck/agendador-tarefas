d0416aad6d5cd72ff883662238dc8c4db41eebe0
Robson Morcineck <robsongabrielengenharia@gmail.com>
2026-05-23 21:34:36 -0300

    adiciona codigo do projeto

---
+++ agendador-tarefas/src/main/java/com/morcineck/agendadortarefas/business/dto/UsuarioDTO.java
@@ -0,0 +1,17 @@
+package com.morcineck.agendadortarefas.business.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+
+}
